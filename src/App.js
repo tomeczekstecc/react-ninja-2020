ca31da009a19a3e5ec616f429e3d8787d9feaa76
Tomasz Steć <tomeczekstecc@gmail.com>
2021-01-22 18:07:37 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new block';
+  const likes = 50;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ]);
+
+  return (
+    <div className='home'>
+      {blogs.length > 0 &&
+        blogs.map((item) => (
+          <div className='blog-preview' key={item.id}>
+            <h2> {item.title}</h2>
+            <p>Written by: {item.author}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Home;
